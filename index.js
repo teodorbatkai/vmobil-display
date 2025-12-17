2a55ba73efe0b02973d786e0c544eeb0c5af73dd
teodorbatkai <teo@teodor-batkai.at>
2025-12-17 20:26:11 +0100

    commit

---
+++ index.js
@@ -0,0 +1,55 @@
+const express = require('express');
+const hafas = require('oebb-hafas')('feldkirch-depboard');
+const app = express();
+const port = 3000;
+
+const STATION_ID = '894306';
+
+app.use(express.static('web'));
+
+app.get('/getdep', async (req, res) => {
+    try {
+        const dep = await hafas.departures(STATION_ID, { duration: 1440, results: 10 });
+        const ret = dep.filter(dep => dep.line && dep.direction)
+            .map(dep => {
+                const now = new Date();
+                const deptime = new Date(dep.when || dep.plannedWhen);
+                let min = Math.floor((deptime - now) / 60000);
+                let asd = min;
+
+                if(min > 40) {
+                    min = `${deptime.getHours()}:${deptime.getMinutes()}`;
+                    if(deptime.getMinutes() < 10) {
+                        min = `${deptime.getHours()}:0${deptime.getMinutes()}`;
+                    }
+                }
+
+                if(now > deptime) {
+                    min = -1;
+                }
+
+                return {
+                    line: dep.line.name.slice(4,7),
+                    destination: dep.direction,
+                    minutes: min,
+                    mm: asd
+                };
+            })
+            .filter(dep => dep.minutes != -1)
+            .sort((a, b) => a.mm - b.mm).slice(0,2);
+
+        console.log("Departures:");
+        console.log(ret);
+
+        res.json(ret);
+
+
+    } catch (error) {
+        console.error("error fetching data", error);
+        res.status(500).json({ error: 'error fetching data' });
+    }
+});
+
+app.listen(port, () => {
+    console.log(`running at http://localhost:${port}`);
+});
---
+++ web/index.js
@@ -0,0 +1,58 @@
+let lines = document.querySelectorAll('.line');
+let dests = document.querySelectorAll('.destination');
+let times = document.querySelectorAll('.time');
+
+let upperdep;
+let lowerdep;
+
+fetchdeps();
+
+setInterval(()=>{fetchdeps();}, 10000);
+
+function fetchdeps(){
+    fetch('/getdep').then(response => response.json()).then(data => {
+        console.log(data);
+        data.forEach((dep, index) => {
+            lines[index].textContent = dep.line;
+            dests[index].textContent = dep.destination;
+
+            if(dep.line == 401) {
+                dests[index].innerHTML = "Katzenturm <a class='via'>via Nofels</a>";
+            }
+
+            if(dep.line == 402) {
+                dests[index].innerHTML = "Bahnhof <a class='via'>via LKH</a>";
+            }
+
+            if(dep.minutes == 0) {
+                if(!((index === 0 && lowerdep) || (index === 1 && upperdep))) {
+                    let state = true;
+                    times[index].innerHTML = '&#9622;';
+                    let dpint = setInterval(()=>{
+                        state = !state;
+                        if(state) times[index].innerHTML = '&#9629;';
+                        else times[index].innerHTML = '&#9622;';
+                    }, 800);
+                    if(index) upperdep = dpint;
+                    else lowerdep = dpint;
+                }
+            } else {
+                if(index && upperdep) {
+                    clearInterval(upperdep);
+                    upperdep = null;
+                }
+                if(!index && lowerdep) {
+                    clearInterval(lowerdep);
+                    lowerdep = null;
+                }
+                times[index].innerHTML = dep.minutes;
+            }
+
+            if(dep.minutes.toString().length > 2){
+                document.querySelectorAll('.row')[index].style.gridTemplateColumns = '40vh 1fr 70vh';
+            }else{
+                document.querySelectorAll('.row')[index].style.gridTemplateColumns = '40vh 1fr 20vh';
+            }
+        });
+    });
+}
